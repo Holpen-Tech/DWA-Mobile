688123b8fa3133208db59feed492db662b0ffc72
Greatness Ransome-Kuti <greatnessrkuti@mac.home>
2025-03-02 03:40:01 -0500

    just a little update to the filter component in 'JobBoard.js' -  as of now, the filters do not work but everything else runs properly.

--- app/Screens/JobBoard.js
+++ app/Screens/JobBoard.js
@@ -25,16 +25,18 @@ export default function JobBoard({ navigation }) {
   const [sortBy, setSortBy] = useState(null);
 
   useEffect(() => {
-    fetch("http://192.168.2.16:3000/api/jobs")
+    fetch("http://YOUR_IP_ADDRESS:3000/api/jobs") // Greatness: chances are, you will get an error, so add your IP address appropriately
       .then((response) => response.json())
       .then((data) => {
-        setJobData(data);
-        setFilteredJobs(data);
+        // Check if data.jobs exists, otherwise use empty array
+        const jobsArray = data.jobs || [];
+        setJobData(jobsArray);
+        setFilteredJobs(jobsArray);
       })
       .catch((err) => setError(err.message))
       .finally(() => setLoading(false));
   }, []);
-
+  
   const applyFilters = () => {
     let filtered = [...jobData];
 
